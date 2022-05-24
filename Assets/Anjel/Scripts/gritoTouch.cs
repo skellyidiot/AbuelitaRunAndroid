dca162fa96aa9112d04e61163e0575f612f1d82c
MUKILTEO\1541675 <kingcreeper7777@gmail.com>
2022-05-24 13:39:10 -0700

    Tried to make stuff work idk i'll prob do it today if im bored

--- Assets/Anjel/Scripts/gritoTouch.cs
+++ Assets/Anjel/Scripts/gritoTouch.cs
@@ -12,29 +12,27 @@ public class gritoTouch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Button btn = button.GetComponent<Button>();
-        //btn.onClick.AddListener(Down);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Button btn = button.GetComponent<Button>();
-
-        //if (btn.onClick == false)
+        if (Input.GetMouseButtonDown(0))
         {
-            //touched = false;
+            RaycastHit2D hitInfo = new RaycastHit2D();
+            int layer_mask = LayerMask.GetMask("UI Interact");
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            hitInfo = Physics2D.Raycast(mousePos2D, Vector2.zero, 100000, layer_mask);
+            Debug.LogError(hitInfo.collider.name);
+
+            if (hitInfo.collider.tag == "GritoUI")
+            {
+                touched = true;
+            } else
+            {
+                touched = false;
+            }
         }
-    }
-
-    public void OnMouseDown(MouseDownEvent a)
-    {
-        Debug.Log("HHHHHH");
-        touched = true;
-    }
-
-    public void OnMouseUp(MouseUpEvent e)
-    {
-        touched = false;
     }
 }
